ebb5bffd38fcc3a89a755ae906ff6152d10aa66f
wangqikang <chicagowang@163.com>
2019-07-16 08:07:35 +0800

    add word count with java and scala.

---
+++ spark/src/main/java/com/wolf/bigdata/spark/wordcount/JavaWordCount.java
@@ -0,0 +1,68 @@
+package com.wolf.bigdata.spark.wordcount;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+/**
+ * @Description java实现worddcount功能，并提交到集群运行
+ * @Author wangqikang
+ * @Date 2019-07-15 23:14
+ */
+public class JavaWordCount {
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("JavaWordCount");
+        // 创建 java spark context
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+        // 读取数据
+        JavaRDD<String> lines = jsc.textFile(args[0]);
+        // 切分
+        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public Iterable<String> call(String line) throws Exception {
+                return Arrays.asList(line.split(" "));
+            }
+        });
+        // 单词计数
+        JavaPairRDD<String, Integer> wordAndOne = words.mapToPair(new PairFunction<String, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(String word) throws Exception {
+                return new Tuple2<String, Integer>(word, 1);
+            }
+        });
+        // 分组聚合
+        JavaPairRDD<String, Integer> result = wordAndOne.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1 + v2;
+            }
+        });
+        // 反转
+        JavaPairRDD<Integer, String> swappedPair = result.mapToPair(new PairFunction<Tuple2<String, Integer>, Integer, String>() {
+            @Override
+            public Tuple2<Integer, String> call(Tuple2<String, Integer> tuple2) throws Exception {
+                return new Tuple2<Integer, String>(tuple2._2, tuple2._1);
+            }
+        });
+        // 排序并反转（只能按key排序）
+        JavaPairRDD<String, Integer> finalResult = swappedPair.sortByKey(false).mapToPair(new PairFunction<Tuple2<Integer, String>, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(Tuple2<Integer, String> tuple2) throws Exception {
+                // 交换 Tuple2 中元素的顺序
+                return tuple2.swap();
+            }
+        });
+        // 保存
+        finalResult.saveAsTextFile(args[1]);
+        // 释放资源
+        jsc.stop();
+    }
+}
---
+++ spark/src/main/scala/com/wolf/bigdata/spark/wordcount/ScalaWordCount.scala
@@ -0,0 +1,39 @@
+package com.wolf.bigdata.spark.wordcount
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * @Description scala实现wordcount功能，并提交到集群上运行
+  * @Author wangqikang
+  * @Date 2019-07-15 23:04
+  */
+object ScalaWordCount {
+
+  /**
+    * main() 作为程序执行的入口
+    *
+    * @param args
+    */
+  def main(args: Array[String]): Unit = {
+    // 创建 SparkConf 并设置 App 名称
+    val conf = new SparkConf().setAppName("WordCount")
+    // 创建 SparkContext，该对象是通向集群的入口
+    val sc = new SparkContext(conf)
+    // 读取 hdfs 中的数据
+    val lines: RDD[String] = sc.textFile(args(0))
+    // 切分单词
+    val words: RDD[String] = lines.flatMap(_.split(""))
+    // 将单词计数
+    val wordAndOne: RDD[(String, Int)] = words.map((_, 1))
+    // reduceByKey 分组聚合，在每个分区上先进行聚合，再进行全局聚合
+    val result: RDD[(String, Int)] = wordAndOne.reduceByKey((x, y) => x + y)
+    // 排序
+    val finalResult: RDD[(String, Int)] = result.sortBy(_._2, false)
+    // 将数据存储到 hdfs 中
+    finalResult.saveAsTextFile(args(1))
+    // 释放资源
+    sc.stop()
+  }
+
+}
