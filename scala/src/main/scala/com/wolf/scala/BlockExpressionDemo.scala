771bab46a2e9eb3e3f144fe3ebe7f40a70731185
wangqikang <chicagowang@163.com>
2019-10-27 22:25:44 +0800

    scala基础语法

---
+++ scala/src/main/scala/com/wolf/scala/BlockExpressionDemo.scala
@@ -0,0 +1,20 @@
+package com.wolf.scala
+
+object BlockExpressionDemo {
+  def main(args: Array[String]): Unit = {
+    val x = 0
+
+    // scala的{}中可包含一系列表达式，最后一个表达式的值即是块的值
+    val res = {
+      if (x < 0) {
+        -1
+      } else if (x >= 1) {
+        1
+      } else {
+        "error"
+      }
+    }
+
+    println("res:" + res) // error
+  }
+}
